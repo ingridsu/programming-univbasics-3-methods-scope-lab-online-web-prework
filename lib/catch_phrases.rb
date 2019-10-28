d2d8eeed3fc61e05697f8c9bff7273a283fd9800
Ingrid sura <sura.ingrid@gmail.com>
2019-10-28 09:56:06 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,6 +1,5 @@
 def mario
-  $status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts status 
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
 end
 
