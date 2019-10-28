cab4454db7e83dd0a6b18a5e3991b44e56b8f218
Ingrid sura <sura.ingrid@gmail.com>
2019-10-28 09:07:09 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,6 @@
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase="It's-a me, Mario!"
-  expect{mario}.to outpu("Its-a me, Mario!\n").to_stdout
- end
+  puts phrase
 end
 
 def toadstool
